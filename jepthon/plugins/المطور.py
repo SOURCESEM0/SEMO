60de64c655b7e086898879a6590c5eafd201ad90
SOURCESEM0 <116640043+SOURCESEM0@users.noreply.github.com>
2022-11-04 15:44:48 +0200

    Update المطور.py

--- jepthon/plugins/المطور.py
+++ jepthon/plugins/المطور.py
@@ -39,15 +39,15 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
     CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
-    CAT_IMG = " https://telegra.ph/file/83d3c9610bc957b20e35a.jpg "
+    CAT_IMG = " https://telegra.ph/file/ec05d05719cc93dd2b663.jpg "
     if CAT_IMG:
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
         PIC = random.choice(A_IMG)
-        cat_caption = f"مطورين الجوكر\n"
+        cat_caption = f"مطورين سورس سيمو\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
-        cat_caption += f"- المطور  : @lMl10l\n"
-        cat_caption += f"- المطور  : @rd0r0\n"
+        cat_caption += f"- المطور  : @DEV_SAMIR\n"
+        cat_caption += f"- القناة  : @SEMO8L\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         await event.client.send_file(
             event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
